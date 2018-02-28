8d000d0885092194e5b7052b2820d8de92e0169c
david1707 <DavidAlacant@gmail.com>
2018-02-28 11:16:18 +0100

    Working. Needs security checks and more group extensions

--- rename_files.py
+++ rename_files.py
@@ -1,22 +1,37 @@
 import os
 
-number = 1
+def rename_files():
+    number = 1
 
-path = input("Path of the files:\n")
-new_name = input("New name for the files:\n")
-extension = input("What type of files do you want to affect?\n").lower()
+    # Get path and make sure it ends with a \
+    path = input("Path of the files:\n")
+    if not path.endswith("\\"):
+        path = path + "\\"
 
-if extension == 'images':
-    extension = ['png', 'jpg', 'bmp', 'jpeg']
-    extensionFinal = input("What type of file do you want to convert to?:\n").lower().replace('.', '')
+    new_name = input("New name for the files:\n")
+    extension = input("What type of files do you want to affect?\n").lower()
 
-else:
-    extensionFinal = extension.replace('.', '')
+    # User can use 'images' to rename every single image despite the extension
+    if extension == 'images':
+        extension = ['png', 'jpg', 'bmp', 'jpeg']
+        extension_final = input("What type of file do you want to convert to?:\n").lower().replace('.', '')
 
-files = [file for file in os.listdir(path) if os.path.isfile(''.join(path+file))]
+    else:
+        extension_final = extension.replace('.', '')
 
-for file in files:
-    if file.split('.',)[1] in extension:
-        print("Entra")  # ToDo
-        os.rename(''.join(path+file), ''.join(path + new_name + "_" + str(number) + "."+extensionFinal))
-        number += 1
+    files = [file for file in os.listdir(path) if os.path.isfile(''.join(path+file))]
+
+    for file in files:
+        if file.split('.',)[1] in extension:
+            os.rename(''.join(path+file), ''.join(path + new_name + "_" + str(number) + "."+extension_final))
+            number += 1
+
+
+while True:
+    rename_files()
+
+    repeat = input("Done.\nDo you want to rename more files? (Y/N)\n").lower()
+    if repeat == "n":
+        break
+
+print("Closing....")
